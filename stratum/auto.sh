e8dcdffd459193bb0a77a4df32af37d6a9038a97
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-25 15:23:38 +0700

    Update auto.sh

--- stratum/auto.sh
+++ stratum/auto.sh
@@ -1,18 +1,26 @@
 sudo apt install -y docker.io npm
+mkdir 
+cd 
 git clone https://github.com/oneevil/stratum-ethproxy
 cd stratum-ethproxy
 npm install
 LOCAL_IP=$(hostname -I | awk '{print $1}')
-input_remote_info
-echo "REMOTE_HOST=hk.pyrin.herominers.com" >> gpu.env
-echo "REMOTE_PORT=1177" >> gpu.env
-echo "REMOTE_PASSWORD=x" >> gpu.env
-echo "LOCAL_HOST=$LOCAL_IP" >> gpu.env
-echo "LOCAL_PORT=443" >> gpu.env
+echo "REMOTE_HOST=hk.pyrin.herominers.com" >> .env
+echo "REMOTE_PORT=1177" >> .env
+echo "REMOTE_PASSWORD=x" >> .env
+echo "LOCAL_HOST=$LOCAL_IP" >> .env
+echo "LOCAL_PORT=443" >> .env
+sudo screen -dmS stratumeth npm start
 
-echo "REMOTE_HOST=nomp.mofumofu.me" >> gula.env
-echo "REMOTE_PORT=3391" >> gula.env
-echo "REMOTE_PASSWORD=x" >> gula.env
-echo "LOCAL_HOST=$LOCAL_IP" >> gula.env
-echo "LOCAL_PORT=80" >> gula.env
+cd ..
+mkdir 
+cd 
+git clone https://github.com/oneevil/stratum-ethproxy
+cd stratum-ethproxy
+npm install
+echo "REMOTE_HOST=nomp.mofumofu.me" >> .env
+echo "REMOTE_PORT=3391" >> .env
+echo "REMOTE_PASSWORD=x" >> .env
+echo "LOCAL_HOST=$LOCAL_IP" >> .env
+echo "LOCAL_PORT=80" >> .env
 sudo screen -dmS stratumeth npm start
