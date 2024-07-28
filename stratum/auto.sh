f592b6b03ed8e7e468b6ef8f6e9df925d2ad4ae6
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-28 16:17:02 +0700

    Update auto.sh

--- stratum/auto.sh
+++ stratum/auto.sh
@@ -1,26 +1,38 @@
+# Install necessary packages
+sudo apt update
 sudo apt install -y docker.io npm
-mkdir gpu
-cd gpu
-git clone https://github.com/oneevil/stratum-ethproxy
-cd stratum-ethproxy
-npm install
-LOCAL_IP=$(hostname -I | awk '{print $1}')
-echo "REMOTE_HOST=hk.pyrin.herominers.com" >> .env
-echo "REMOTE_PORT=1177" >> .env
-echo "REMOTE_PASSWORD=x" >> .env
-echo "LOCAL_HOST=$LOCAL_IP" >> .env
-echo "LOCAL_PORT=443" >> .env
-sudo screen -dmS stratumeth npm start
 
-cd ..
-mkdir gula
-cd gula
+# Create and set up the 'gpu' directory
+mkdir Pointing
+cd Pointing
 git clone https://github.com/oneevil/stratum-ethproxy
 cd stratum-ethproxy
 npm install
-echo "REMOTE_HOST=nomp.mofumofu.me" >> .env
-echo "REMOTE_PORT=3391" >> .env
-echo "REMOTE_PASSWORD=x" >> .env
-echo "LOCAL_HOST=$LOCAL_IP" >> .env
-echo "LOCAL_PORT=80" >> .env
-sudo screen -dmS stratumeth npm start
+
+# Set environment variables for 'gpu'
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+cat <<EOL >> .env
+REMOTE_HOST=hk.pyrin.herominers.com
+REMOTE_PORT=1177
+REMOTE_PASSWORD=x
+LOCAL_HOST=$LOCAL_IP
+LOCAL_PORT=443
+EOL
+
+# Start the stratum-ethproxy in a detached screen session with a specific name
+sudo screen -dmS stratumeth_gpu npm start
+
+# Reset .env file for 'gula'
+> .env
+
+# Set environment variables for 'gula'
+cat <<EOL >> .env
+REMOTE_HOST=nomp.mofumofu.me
+REMOTE_PORT=3391
+REMOTE_PASSWORD=x
+LOCAL_HOST=$LOCAL_IP
+LOCAL_PORT=80
+EOL
+
+# Start the stratum-ethproxy in a detached screen session with a specific name
+sudo screen -dmS stratumeth_gula npm start
