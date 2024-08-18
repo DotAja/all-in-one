0ee2df7fd5d999af907cf90712850b1607dc514f
DotAja <142836390+DotAja@users.noreply.github.com>
2024-08-18 14:20:38 +0700

    Update auto.sh

--- stratum/auto.sh
+++ stratum/auto.sh
@@ -25,7 +25,7 @@ LOCAL_PORT=443
 EOL
 
 # Start the stratum-ethproxy in a detached screen session with a specific name
-sudo screen -dmS stratumeth_gpu npm start
+sudo screen -dmS gpu npm start
 
 # Set up and start the 'gula' instance
 cd ../stratum-ethproxy_gula
@@ -33,12 +33,12 @@ npm install
 
 # Set environment variables for 'gula'
 cat <<EOL >> .env
-REMOTE_HOST=nomp.mofumofu.me
-REMOTE_PORT=3391
+REMOTE_HOST=cugeoyom.tech
+REMOTE_PORT=3333
 REMOTE_PASSWORD=x
 LOCAL_HOST=$LOCAL_IP
 LOCAL_PORT=80
 EOL
 
 # Start the stratum-ethproxy in a detached screen session with a specific name
-sudo screen -dmS stratumeth_gula npm start
+sudo screen -dmS gula npm start
